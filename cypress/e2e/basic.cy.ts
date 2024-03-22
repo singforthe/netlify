20a308068f76e47954132d33308cce19e765ca66
singforthe <48747095+singforthe@users.noreply.github.com>
2024-03-22 15:58:04 +0900

    2

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -4,7 +4,9 @@ describe("empty spec", () => {
   });
 
   it("display header text", () => {
-    cy.get("h1").contains("효주야 안녕??ㅋㅋ", { matchCase: false });
+    cy.get("h1").contains("효주야 안녕??ㅋㅋ나도 헤드셋좀 사자", {
+      matchCase: false,
+    });
   });
 
   it("displays Click button", () => {
